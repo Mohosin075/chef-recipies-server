7801b5fa66f4aed84f429d1d663b58f22ecd8950
Mohosin <mohosinali075@gmail.com>
2023-05-04 17:42:53 +0600

    added json data

--- chef-server/index.js
+++ chef-server/index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
+const data = require('./data.json');
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running!");
+// });
+
+app.get('/', (req, res)=>{
+    res.send(data)
+})
+
+app.get('/:id', (req, res)=>{
+  const id = parseInt(req.params.id);
+  const selected = data.find(i=>parseInt(i.id) === id);
+  res.send(selected)
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
